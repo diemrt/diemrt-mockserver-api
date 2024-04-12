957d93fd7fb35165be5f7af96f65be7ec095f1bc
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-04-12 11:27:28 +0200

    Add some doc

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+//Inizializzo in modo standard express
 const express = require('express')
 const app = express()
 const port = 3000
 
+//Carico le configurazioni nel file .env
+//Nel file deve essere presente la variabile JWT_TOKEN=...
+const dotenv = require('dotenv')
+dotenv.config()
+
+//Aggiungo un esempio di chiamata GET di una lista fittizia di post.
 app.get('/posts', (req, res) => {
   const posts = {
     data: [
@@ -15,6 +22,8 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
+//Aggiungo un esempio di chiamata GET per avere un singolo post. L'id inserito simula l'id del post richiesto.
+//La chiamata può andare volutamente in errore se si inserisce l'id 2 nella richiesta
 app.get('/posts/:postId', (req, res) => {
   const posts = {
     data: 
@@ -31,24 +40,28 @@ app.get('/posts/:postId', (req, res) => {
   res.send(posts)
 })
 
+//Aggiungo un esempio di chiamata POST, che ritorna un 201Created
 app.post('/posts', (req, res) => {  
   res.status(201).end()
   res.send()
 })
 
+//Aggiungo un esempio di chiamata PUT
 app.put('/posts/:postId', (req, res) => {  
   res.send()
 })
 
+//Aggiungo un esempio di chiamata PATCH
 app.patch('/posts/:postId', (req, res) => {  
   res.send()
 })
 
+//Aggiungo un esempio di chiamata DELETE
 app.delete('/posts/:postId', (req, res) => {  
   res.send()
 })
 
-
+//Avvio il server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
